248ab2b05ce3945005292d7bf5fb39e71ccb8d2c
Amit Erandole <amit.erandole@gmail.com>
2015-04-19 07:50:48 +0530

    added basic class and mongo config

--- oop/person.rb
+++ oop/person.rb
@@ -1,4 +1,19 @@
 
 class Person
-	
+
+  attr_accessor :age, :name
+
+  def initialize(name, age = 22)
+    @name = name
+    @age = age
+  end
+
 end
+
+bob = Person.new("Bob")
+# p bob.name
+bob.age = 32
+# p bob.age
+
+steve = Person.new("Steve", 22)
+# p steve.name
